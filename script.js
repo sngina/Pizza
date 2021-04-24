3227f2b28b6b84a313d24dbca9a6982c82ed4380
sngina <sngina707@gmail.com>
2021-04-24 11:22:26 +0300

    add a table

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
+//business logic
+function size(extra , large , medium , small) {
+    this.elarge = elarge;
+    this.large = large;
+    this.medium = medium;
+    this.small = small
+};
+function topping(topping1,topping2){
+    this.topping = topping1;
+    this.topping = topping2;
+};
+    
+
+
+
+
+
+
+
+
+
+
 //delivery option getting a prompt to add your address.
 $(document).ready(function(){
     $("#deliver").click
-    prompt("Please add your location")
+    prompt("Please add your address")
     alert("Thank you!Oder received!")
-})
+});
