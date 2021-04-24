2a0aeebe75be305a4669650bac0cb913396bd307
sngina <sngina707@gmail.com>
2021-04-24 10:44:38 +0300

    add a function

---
+++ script.js
@@ -0,0 +1,6 @@
+//delivery option getting a prompt to add your address.
+$(document).ready(function(){
+    $("#deliver").click
+    prompt("Please add your location")
+    alert("Thank you!Oder received!")
+})
