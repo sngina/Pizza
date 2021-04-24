6c7228f231575366e8b4284e754a893d88e46453
sngina <sngina707@gmail.com>
2021-04-24 11:59:41 +0300

    chang background photo

--- script.js
+++ script.js
@@ -5,24 +5,13 @@ function size(extra , large , medium , small) {
     this.medium = medium;
     this.small = small
 };
-function topping(topping1,topping2){
-    this.topping = topping1;
-    this.topping = topping2;
-};
-    
-
-
-
-
-
-
-
-
-
 
 //delivery option getting a prompt to add your address.
 $(document).ready(function(){
-    $("#deliver").click
+    $("#deliver").click(function(event){
+    event.preventDefault();
+    
+    });
     prompt("Please add your address")
     alert("Thank you!Oder received!")
 });
